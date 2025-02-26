69675290e87a301645d400565528080bbfd4f24f
Breno Alves <breno202nemezes@gmail.com>
2025-02-26 13:50:07 -0300

    Parte 3 e 4 colocadas

--- Projeto_01.js
+++ Projeto_01.js
@@ -1,20 +1,29 @@
 //Teste de projeto git commit -a -m "ALTERAÇÃO"
 //                 git push origin master
 
-//PARTE-1 [DEFINIÇÃO DO REGISTRO]
-const aluno =(nome,idade,matricula,curso)=>Object.freeze({nome, idade, matricula, curso})
+// PARTE-1 [DEFINIÇÃO DO REGISTRO]
+const aluno = (nome, idade, matricula, curso) => Object.freeze({ nome, idade, matricula, curso });
 
-//PARTE-2[CRIAÇÃO DO REGISTRO]
-const turma =Object.freeze([
-    criarAluno("Helen",22,2021000,"CC"),
-    criarAluno("Paula",20,2022001,"SI"),
-    criarAluno("Breno",22,2023002,"EC"),
-    criarAluno("Kalil",78,1978003,"CC")
-])
+// PARTE-2 [CRIAÇÃO DO REGISTRO]
+const turma = Object.freeze([
+    aluno("Helen", 22, 2021000, "CC"),
+    aluno("Paula", 20, 2022001, "SI"),
+    aluno("Breno", 22, 2023002, "EC"),
+    aluno("Kalil", 78, 1978003, "CC")
+]);
 
-//PARTE-3 [ADIÇÃO DE ALUNOS]
+// PARTE-3 [ADIÇÃO DE ALUNOS]
+const adicionarAluno = (nome, idade, matricula, curso) => {
+    const alunoNovo = aluno(nome, idade, matricula, curso);
+    return Object.freeze([...turma, alunoNovo]);
+};
 
-//PARTE-4 [LISTAGEM DE ALUNOS]
+// PARTE-4 [LISTAGEM DE ALUNOS]
+const listagemAlunos = (turma) => {
+    turma.forEach(aluno => {
+        console.log(`Nome: ${aluno.nome}, Idade: ${aluno.idade}, Matrícula: ${aluno.matricula}, Curso: ${aluno.curso}`);
+    });
+};
 
 //PARTE-5 [BUSCA POR CURSO]
 
